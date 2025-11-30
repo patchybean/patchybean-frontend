9ae127a9d5cf51a25ee961c80163d41245c9e6c1
Bean <dev@patchybean.com>
2025-11-30 17:04:29 +0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white font-sans text-black">
+      <main className="text-3xl font-semibold tracking-tight">
+        Hi, I'm Patchy Bean!
+      </main>
+    </div>
+  );
+}
