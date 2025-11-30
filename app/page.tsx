1b93ac9c4dc6dd1567f9bd879056ad3c7d68a8cd
Bean <dev@patchybean.com>
2025-11-30 17:18:20 +0700

    feat: add blank hello world page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-white font-sans text-black">
       <main className="text-3xl font-semibold tracking-tight">
-        Hi, I'm Patchy Bean!
+        hello world, i'm patchy bean
       </main>
     </div>
   );
